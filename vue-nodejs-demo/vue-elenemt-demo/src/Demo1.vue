e7f71a7ed0b37385fefb1ec8573316d281076b99
shashijie <87419590@qq.com>
2024-08-01 08:58:04 +0800

    整理

--- vue-nodejs-demo/vue-elenemt-demo/src/Demo1.vue
+++ vue-nodejs-demo/vue-elenemt-demo/src/Demo1.vue
@@ -1,48 +1,67 @@
 <template>
-    <!-- 表单搜索 -->
-    <el-form :inline="true" :model="searchConditions" class="demo-form-inline">
-        <!-- 文本 -->
-        <el-form-item label="分类">
-            <!-- placeholder: 用于输入框内灰色的占位字符的设置，例：placeholder="请输入用户名",需要配合 clearable 属性一起使用
+    <!-- 整体卡片 -->
+    <el-card style="max-width: 100%">
+        <!-- 头部 -->
+        <div class="card-header">
+            <span>文章管理</span>
+            <el-button type="primary" @click="">发布文章</el-button>
+        </div>
+
+        <div style="margin-top: 20px;">
+            <hr>
+        </div>
+
+        <!-- 表单搜索 -->
+        <el-form :inline="true" :model="searchConditions" class="demo-form-inline">
+            <!-- 文本 -->
+            <el-form-item label="标题">
+                <!-- placeholder: 用于输入框内灰色的占位字符的设置，例：placeholder="请输入用户名",需要配合 clearable 属性一起使用
                  clearable: 是否可清空,默认false
             -->
-            <el-input v-model="searchConditions.category" placeholder="请输入" clearable />
-        </el-form-item>
-        <el-form-item label="状态">
-            <el-input v-model="searchConditions.state" placeholder="请输入" clearable />
-        </el-form-item>
-        <!-- 下拉框 -->
-        <!-- <el-form-item label="Activity zone">
-            <el-select v-model="searchConditions.region" placeholder="Activity zone" clearable>
-                <el-option label="Zone one" value="shanghai" />
-                <el-option label="Zone two" value="beijing" />
-            </el-select>
-        </el-form-item> -->
-        <!-- 日期框 -->
-        <!-- <el-form-item label="Activity time">
-            <el-date-picker v-model="searchConditions.date" type="date" placeholder="Pick a date" clearable />
-        </el-form-item> -->
-        <el-form-item>
-            <el-button type="primary" @click="onSubmit">搜索</el-button>
-        </el-form-item>
-        <el-form-item>
-            <el-button type="default" @click="clear">重制</el-button>
-        </el-form-item>
-    </el-form>
+                <el-input v-model="searchConditions.title" placeholder="请输入" clearable />
+            </el-form-item>
+            <!-- <el-form-item label="状态">
+                <el-input v-model="searchConditions.state" placeholder="请输入" clearable />
+            </el-form-item> -->
+            <!-- 下拉框 -->
+            <el-form-item label="分类">
+                <el-select v-model="searchConditions.category" placeholder="请选择" clearable>
+                    <el-option label="1-时事" value="时事" />
+                    <el-option label="2-旅游" value="旅游" />
+                    <el-option label="3-篮球" value="篮球" />
+                </el-select>
+            </el-form-item>
+            <el-form-item label="状态">
+                <el-select v-model="searchConditions.state" placeholder="请选择" clearable>
+                    <el-option label="1-已发布" value="已发布" />
+                    <el-option label="2-草稿" value="草稿" />
+                </el-select>
+            </el-form-item>
+            <!-- 日期框 -->
+            <el-form-item label="发布日期">
+                <el-date-picker v-model="searchConditions.time" type="date" placeholder="请选择" clearable value-format="YYYY-MM-DD"/>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="onSubmit">搜索</el-button>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="default" @click="clear">重制</el-button>
+            </el-form-item>
+        </el-form>
 
-    <!-- 表格 -->
-    <el-table :data="articleList" style="width: 100%">
-        <el-table-column prop="title" label="文章标题" />
-        <el-table-column prop="category" label="分类" />
-        <el-table-column prop="time" label="发表时间" />
-        <el-table-column prop="state" label="状态" />
-        <el-table-column label="操作" width="180">
-            <el-button type="primary" :icon="Edit" circle />
-            <el-button type="danger" :icon="Delete" circle />
-        </el-table-column>
-    </el-table>
+        <!-- 表格 -->
+        <el-table :data="articleList" style="width: 100%">
+            <el-table-column prop="title" label="文章标题" />
+            <el-table-column prop="category" label="分类" />
+            <el-table-column prop="time" label="发表时间" />
+            <el-table-column prop="state" label="状态" />
+            <el-table-column label="操作" width="180">
+                <el-button type="primary" :icon="Edit" circle />
+                <el-button type="danger" :icon="Delete" circle />
+            </el-table-column>
+        </el-table>
 
-    <!-- 分页条 
+        <!-- 分页条 
             layout: 控制分页元素显示顺序 jumper:前往第几页
                                         total:总记录数
                                         sizes:每页多少条下拉框
@@ -56,11 +75,12 @@
             size: 分页样式大小,三种选择'large'| 'default'| 'small'
             @size-change: 修改下拉框每页显示条数回调方法
             @current-change: 前往第几页,或点击页码超链接回调方法
-    -->
-    <el-pagination class="el-p" v-model:current-page="currentPage4" v-model:page-size="pageSize4"
-        :page-sizes="[10, 20, 30, 40]" :size="size" :disabled="disabled" :background="background"
-        layout="jumper ,total, sizes, prev, pager, next" :total="total" @size-change="handleSizeChange"
-        @current-change="handleCurrentChange" />
+        -->
+        <el-pagination class="el-p" v-model:current-page="currentPage4" v-model:page-size="pageSize4"
+            :page-sizes="[10, 20, 30, 40]" :size="size" :disabled="disabled" :background="background"
+            layout="jumper ,total, sizes, prev, pager, next" :total="total" @size-change="handleSizeChange"
+            @current-change="handleCurrentChange" />
+    </el-card>
 </template>
 
 <script lang="ts" setup>
@@ -122,8 +142,10 @@ axios.post("http://localhost:8080/articleController/getAll").then(result => {
 /** 表单搜索 */
 // 定义响应式数据 条件对象
 const searchConditions = ref({
+    title: "",
     category: "",
-    state: ""
+    state: "",
+    time: ""
 });
 /** 搜索事件 */
 const onSubmit = () => {
@@ -144,8 +166,10 @@ const onSubmit = () => {
 /** 重制事件 */
 const clear = () => {
     // 保存响应式数据一定要用.value,不能直接赋值
+    searchConditions.value.title = "";
     searchConditions.value.category = "";
     searchConditions.value.state = "";
+    searchConditions.value.time = "";
 };
 </script>
 
@@ -169,5 +193,13 @@ const clear = () => {
 /**表单下拉框样式 */
 .demo-form-inline .el-select {
     --el-select-width: 220px;
+}
+
+/** 卡片样式 */
+.card-header {
+    /* 弹性布局（Flex布局）是一种现代的CSS布局方式，通过使用display: flex属性来创建一个弹性容器，并在其中使用灵活的盒子模型来进行元素的排列和定位。 */
+    display: flex;
+    /* 弹性盒子容器的对齐方式: space-between 两端对齐 中间等距*/
+    justify-content: space-between;
 }
 </style>
